5bd118288bc1396658eddc8eb1ca122da4dcdf71
kurgyisp10 <kurgyis.p@gmail.com>
2020-05-22 22:20:57 +0200

    Build foldery

---
+++ build/precache-manifest.e5943048f67ae8485e6f9fe64a863216.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e55e374844268ea2aa3be9a2b0541ce4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3132671749c16290edd1",
+    "url": "./static/css/main.98b00dfd.chunk.css"
+  },
+  {
+    "revision": "07c155597dc075058377",
+    "url": "./static/js/2.821ad3b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.821ad3b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3132671749c16290edd1",
+    "url": "./static/js/main.788b5bf5.chunk.js"
+  },
+  {
+    "revision": "763f551d24bf50b1938c",
+    "url": "./static/js/runtime-main.c7b6dbd4.js"
+  }
+]);
